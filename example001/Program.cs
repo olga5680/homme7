8cb6b8c6d5b44bf36439ab7f7968c7e68ee1651c
OLGA <1986o2019@mail.ru>
2023-07-18 19:43:48 +0300

    example001

---
+++ example001/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число A: ");
+  int A = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int B = Convert.ToInt32(Console.ReadLine());
+  int result = 1;
+  for(int i=1; i <= B; i++){
+    result = result * A;
+  }
+    Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow(A*A, B))));
